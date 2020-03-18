7095ac89a6b6172ff3292df41cbac071479e1d21
Dinesh jangir <40563654+DineshJangir900@users.noreply.github.com>
2020-03-18 21:49:54 +0530

    Update ExampleService.java

--- app/src/main/java/com/example/forground/ExampleService.java
+++ app/src/main/java/com/example/forground/ExampleService.java
@@ -3,9 +3,12 @@ package com.example.forground;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.media.MediaRecorder;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Vibrator;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
@@ -16,46 +19,47 @@ import java.io.IOException;
 import static com.example.forground.App.CHANNEL_ID;
 
 public class ExampleService extends Service {
-    double aData;
-    Thread thread;
-    String input = "12";
-    MediaRecorder recorder;
+    String input;
     Notification notification;
+    MediaRecorder recorder;
+    double amplitude,value;
+    private boolean isThreadRun = true;
+    Thread thread;
+    static final private double EMA_FILTER = 0.6;
+    private static double mEMA = 0.0;
+
     @Override
     public void onCreate() {
         super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(final Intent intent, int flags, int startId) {
         recorder = new MediaRecorder();
         recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
         recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
         recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
         recorder.setOutputFile("dev/null");
-        try {
+        try{
             recorder.prepare();
-        } catch (IOException e) {
+        }catch (IOException e) {
             e.printStackTrace();
         }
         recorder.start();
 
-        thread = new Thread() {
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(500);
-                    aData = recorder.getMaxAmplitude();
-                    input =String.valueOf(aData);
-                    Log.d("ccc",input);
-                } catch (InterruptedException e) {
+
+        startListenAudio();
+            if (value >= 60) {
+                if (value > 60) {
+                    if (value >= 61) {
+                        if (value >= 65) {
+                            Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                            long vibratePattern[] = {0, 5000};
+                            v.vibrate(vibratePattern, 1);
+                        }
+                    }
                 }
             }
-        };
-        thread.start();
-
-
-
-    }
-
-    @Override
-    public int onStartCommand(final Intent intent, int flags, int startId) {
 
 
         Intent notificationIntent = new Intent(this,MainActivity.class);
@@ -63,22 +67,17 @@ public class ExampleService extends Service {
                 0,notificationIntent,0);
 
         notification = new NotificationCompat.Builder(this,CHANNEL_ID)
-                .setContentText("Forground Service")
+                .setOngoing(true)
                 .setSmallIcon(R.drawable.ic_launcher_background)
                 .setContentIntent(pendingIntent)
                 .build();
 
         startForeground(1,notification);
-
         return START_STICKY;
     }
 
-
     @Override
     public void onDestroy() {
-        recorder.stop();
-        recorder.release();
-        recorder = null;
         super.onDestroy();
     }
 
@@ -86,5 +85,38 @@ public class ExampleService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    private double convertdDb(double amplitude) {
+        double mEMAValue = EMA_FILTER * amplitude + (1.0 - EMA_FILTER) * mEMA;
+        return 20 * (float) Math.log10((mEMAValue/51805.5336)/ 0.000028251);
+    }
+
+    private double startListenAudio() {
+        thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (isThreadRun) {
+                    try {
+                        amplitude = getAmplitude();
+                        if(amplitude > 0 && amplitude < 1000000) {
+                            value = convertdDb(amplitude);
+                            input = String.valueOf(value);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        thread.start();
+        return value;
+
+    }
+    public double getAmplitude() {
+        if (recorder != null)
+            return  (recorder.getMaxAmplitude());
+        else
+            return 0;
     }
 }
