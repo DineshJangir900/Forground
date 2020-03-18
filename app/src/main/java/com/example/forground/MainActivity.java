8c4526c610babb8086dd7d18d8571fc8c38000ad
Dinesh jangir <40563654+DineshJangir900@users.noreply.github.com>
2020-03-18 21:50:33 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/forground/MainActivity.java
+++ app/src/main/java/com/example/forground/MainActivity.java
@@ -1,38 +1,67 @@
 package com.example.forground;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.TextView;
 
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-    EditText editTextInput;
-    String input;
-    double aData;
+    private int MY_PERMISSIONS_REQUEST_RECORD_AUDIO = 1;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editTextInput =findViewById(R.id.edit_text_input);
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECORD_AUDIO)) {
+                new AlertDialog.Builder(this)
+                        .setTitle("Permission needed")
+                        .setMessage("This permission is needed for work")
+                        .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.RECORD_AUDIO}, MY_PERMISSIONS_REQUEST_RECORD_AUDIO);
+                            }
+                        })
+                        .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.dismiss();
+                            }
+                        }).create().show();
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, MY_PERMISSIONS_REQUEST_RECORD_AUDIO);
+            }
+        } else {
 
-            input=editTextInput.getText().toString();
-            Intent intent = new Intent(this,ExampleService.class);
-            // intent.putExtra("InputExtra",input);
-            startService(intent);
+        }
+        textView = findViewById(R.id.textview);
+        Intent intent = new Intent(this,ExampleService.class);
+        startService(intent);
 
     }
-
     public void stopService(View v){
         Intent intent = new Intent(this,ExampleService.class);
         stopService(intent);
     }
-
-
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 
 }
